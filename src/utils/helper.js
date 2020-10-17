ca3671cf1f3af02896c7a9545346e3d6f566d625
Carlos Sanchez <carlos.sanchez.a0@gmail.com>
2020-10-17 13:41:04 -0500

    confirmar

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,6 +2,14 @@ import Swal from 'sweetalert2'
 
 export const mensaje = (title='',message='',icon='info')=>Swal.fire(title,message,icon)
 
+export const confirmar = (title='Estás seguro?') => Swal.fire({
+    title: '',
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: `Si`,
+    denyButtonText: `No`,
+})
+
 export default {
     mensaje
 }
